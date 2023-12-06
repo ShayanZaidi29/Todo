00f9f2a2dc7c30f7b4b527469eb09cafb7eea28c
Shayan <biffgarfield21@gmail.com>
2023-12-06 14:38:27 +0530

    Sidebar animation added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,96 +1,86 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+// import SearchIcon from '@mui/icons-material/Search';
+
 
 const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
 
   const navbarStyle = {
     backgroundColor: 'black',
-    padding: '10px',
+    padding: '3px',
     color: 'white',
     display: 'flex',
-    justifyContent: 'space-between',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-  };
-
-  const spaceDivStyle = {
-    marginRight: '10px', // Reduce the space between "TodoList" and the hamburger menu
+    position: 'relative', // Added position relative
   };
 
   const hamburgerStyle = {
     cursor: 'pointer',
-    fontSize: '24px',
-    display: 'block',
-    transition: 'transform 0.6s ease-in-out', // Slow-motion for rotation animation
+    fontSize: '27px',
+    marginRight: '10px',
+    zIndex: 1, // Ensure the hamburger is above other elements
+    transition: 'transform 0.3s ease-in-out', // Add transition for rotation animation
     transform: isMenuOpen ? 'rotate(90deg)' : 'rotate(0deg)', // Rotate when open
   };
 
-  const hamburgerIconStyle = {
-    display: 'block',
+  const sidebarOpenStyle = {
+    width: '200px',
   };
 
   const linkContainerStyle = {
     display: 'flex',
-    alignItems: 'center',
-    position: 'relative',
+    flexDirection: 'column',
+    position: 'absolute',
+    top: '60px',
+    left: isMenuOpen ? '0' : '-200px',
+    transition: 'left 0.6s ease-in-out',
+    zIndex: 0, // Ensure the links container is behind other elements
+    backgroundColor: 'black',
   };
 
   const linkStyle = {
     color: 'white',
+
     textDecoration: 'none',
-    marginRight: '50px', // Space between hamburger menu and links
-    display: 'block',
-    position: 'absolute',
-    top: '40px',
-    backgroundColor: 'black',
     padding: '10px',
-    borderRadius: '5px', // Add border-radius for rounded corners
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Add a subtle box shadow for depth
-    transition: 'opacity 0.6s ease-in-out, transform 0.3s ease-in-out, background-color 0.3s ease-in-out, color 0.3s ease-in-out', // Add transitions for opacity, transform, background-color, and color
-    opacity: isMenuOpen ? 1 : 0, // Show links when open, hide when closed
-    transform: isMenuOpen ? 'translateY(0)' : 'translateY(-10px)', // Lift links slightly when menu is closed
+    transition: 'background-color 0.3s ease-in-out, color 0.3s ease-in-out',
   };
-  
-  // Add hover effect
+
   linkStyle[':hover'] = {
-    backgroundColor: '#333', // Darken background on hover
-    color: 'white', // Change text color to white on hover
+    backgroundColor: '#333',
+    color: 'white',
   };
-  
-  // ... (rest of the code)
-  
-  // In the component, use a library like styled-components or Emotion to handle pseudo-classes
-  // Example with styled-components:
-  
-  
-  const StyledLink = styled(Link)`
-    ${linkStyle}
-  `;
-  
-  // Then use StyledLink in your component instead of Link:
-  // <StyledLink to="/" style={hamburgerIconStyle}>Home</StyledLink>
-  // <StyledLink to="/about" style={hamburgerIconStyle}>About</StyledLink>
-  // <StyledLink to="/contact" style={hamburgerIconStyle}>Contact Us</StyledLink>
-  
+
+  const contentContainerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+  };
+
   const todoListStyle = {
-    marginRight: '1200px'
+    marginRight: '1250px',
+    zIndex: 1, // Ensure the TodoList is above other elements
   };
 
   const searchContainerStyle = {
     display: 'flex',
     alignItems: 'center',
+    marginLeft: 'auto', // Align the search bar to the right
+    marginRight: isMenuOpen ? '200px' : '5px', // Add margin-right only when the menu is open
+    transition: 'margin-right 0.3s ease-in-out', // Added transition for smooth movement
   };
-
+  
   const searchInputStyle = {
-    marginRight: '10px',
+    marginRight: '-25px',
     padding: '5px',
   };
 
   const searchIconStyle = {
-    fill: 'white',
-    width: '20px',
-    height: '20px',
+    fill: isMenuOpen ? 'black' : 'black', // Change icon color based on the menu state
+    width: '25px',
+    height: '25px',
     cursor: 'pointer',
   };
 
@@ -101,28 +91,33 @@ const Navbar = () => {
   return (
     <div>
       <div style={navbarStyle}>
+        <div style={{ ...hamburgerStyle, ...isMenuOpen && sidebarOpenStyle }} onClick={toggleMenu}>
+          &#9776; {/* Hamburger icon */}
+        </div>
+
         <div style={linkContainerStyle}>
-          <div style={hamburgerStyle} onClick={toggleMenu}>
-            &#9776; {/* Hamburger icon */}
-          </div>
-          <div style={linkStyle}>
-            <Link to="/" style={hamburgerIconStyle}>Home</Link>
-            <Link to="/about" style={hamburgerIconStyle}>About</Link>
-            <Link to="/contact" style={hamburgerIconStyle}>Contact Us</Link>
-          </div>
+          <Link to="/" style={linkStyle} onClick={toggleMenu}>
+            Home
+          </Link>
+          <Link to="/about" style={linkStyle} onClick={toggleMenu}>
+            About
+          </Link>
+          <Link to="/contact" style={linkStyle} onClick={toggleMenu}>
+            Contact Us
+          </Link>
         </div>
 
-        <div style={spaceDivStyle}></div>
+        <div style={contentContainerStyle}>
+          <div style={todoListStyle}>
+            <p>TodoList</p>
+          </div>
 
-        <div style={todoListStyle}>
-          <p>TodoList</p>
-        </div>
-
-        <div style={searchContainerStyle}>
-          <input type="text" placeholder="Search" style={searchInputStyle} />
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style={searchIconStyle}>
-            <path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25L20.71 18l-4.25-4.25zM8 14a5 5 0 1 1 0-10 5 5 0 0 1 0 10z"/>
-          </svg>
+          <div style={searchContainerStyle}>
+            <input type="text" placeholder="Search" style={searchInputStyle} />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style={searchIconStyle}>
+              <path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25L20.71 18l-4.25-4.25zM8 14a5 5 0 1 1 0-10 5 5 0 0 1 0 10z" />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
